7fd13c8622f9cc5ba8ad5e9879e2aa78c4912c94
Alexey Chekalin <82474162+alexeychekalin@users.noreply.github.com>
2025-07-18 12:34:49 +0300

    add login path

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -27,5 +28,22 @@ class AuthController extends Controller
             'user'=>$user,
             'token'=> $token
         ]);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+            'device_name' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response('Login invalid', 503);
+        }
+
+        return $user->createToken($request->device_name)->plainTextToken;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -35,7 +35,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'store']);
 Route::middleware('auth:sanctum')->group(function (){
     Route::get('/user', function (Request $request){
         return $request->user();
